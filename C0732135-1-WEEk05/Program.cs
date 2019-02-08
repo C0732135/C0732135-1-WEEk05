5f318cf44392341c5f92cd7dee96b72bc1afb917
visualstudio <visualstudio@122-PC06>
2019-02-08 17:39:53 -0500

    Getting started

---
+++ C0732135-1-WEEk05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0732135_1_WEEk05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
